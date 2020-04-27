f2243f3281f74185e8a0020508296341c39d2514
Alex Mathew <alexmathew003@gmail.com>
2020-04-27 22:29:41 +0530

    Use celery to run the screenshot function async

--- api/pup/models.py
+++ api/pup/models.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.core.validators import URLValidator
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from pup.utils.screenshot.twitter import Twitter
+from pupcket.celery import app
 
 
 class SavedMoment(models.Model):
@@ -13,3 +18,15 @@ class SavedMoment(models.Model):
 
     class Meta:
         ordering = ["-created_date"]
+
+
+@receiver(post_save, sender=SavedMoment)
+def save_screenshot_of_moment(sender, instance=None, created=False, **kwargs):
+    if created:
+        take_screenshot.delay(instance.url)
+
+
+@app.task
+def take_screenshot(url):
+    twitter = Twitter(url)
+    twitter.generate_screenshot()
