84a240311d81176028dda9bf81bc692f64470021
Alex Mathew <alexmathew003@gmail.com>
2020-06-11 23:11:27 +0530

    Show page action on twitter.com

--- extension/src/background.js
+++ extension/src/background.js
@@ -3,6 +3,18 @@ import { fetchMoments } from "./utils/pupcket";
 chrome.runtime.onInstalled.addListener(() => {
   console.log("onInstalled...");
   chrome.alarms.create("refresh", { periodInMinutes: 10 });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "twitter.com" },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
 });
 
 chrome.alarms.onAlarm.addListener(() => {
