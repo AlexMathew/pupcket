861ec5f6b46f976ed296f5bd5d37c7d6e9c35713
Alex Mathew <alexmathew003@gmail.com>
2020-06-13 13:12:52 +0530

    Add sample background layout for new tabs

---
+++ extension/src/components/Newtab.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Newtab extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="container"></div>
+        <div className="center"></div>
+      </div>
+    );
+  }
+}
+
+export default Newtab;
