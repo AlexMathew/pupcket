aa706dfe2363e82b770638d5a81fe95cc9550ff2
Alex Mathew <alexmathew003@gmail.com>
2020-06-13 13:26:50 +0530

    Use a random screenshot from prefetched moments for every new tab

--- extension/src/components/Newtab.js
+++ extension/src/components/Newtab.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { MOMENTS_STORAGE_FIELD } from "../constants";
 
 class Newtab extends React.Component {
+  state = {
+    image: "",
+  };
+
+  componentDidMount() {
+    chrome.storage.local.get(MOMENTS_STORAGE_FIELD, (result) => {
+      const moments = result[[MOMENTS_STORAGE_FIELD]];
+      const imageIndex = Math.floor(Math.random() * moments.length);
+      this.setState({ image: moments[imageIndex].screenshot_url });
+    });
+  }
+
   render() {
     return (
       <div>
-        <div className="container"></div>
-        <div className="center"></div>
+        <div
+          className="container"
+          style={{ backgroundImage: `url(${this.state.image})` }}
+        ></div>
+        <div
+          className="center"
+          style={{ backgroundImage: `url(${this.state.image})` }}
+        ></div>
       </div>
     );
   }
