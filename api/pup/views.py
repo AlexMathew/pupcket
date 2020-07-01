a3475047221381bd384530ac4a78b45b9ab78b71
Alex Mathew <alexmathew003@gmail.com>
2020-07-01 22:48:34 +0530

    Create random as a new action in the view
    Now supports both normal listing and random fetch

--- api/pup/views.py
+++ api/pup/views.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from rest_framework import mixins, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import SavedMoment
 from .serializers import SavedMomentSerializer
@@ -12,10 +14,8 @@ class SavedMomentView(
 
     def get_queryset(self):
         if self.action in ["list"]:
-            return (
-                SavedMoment.random.screenshot_generated()
-                .owned_by(self.request.user)
-                .pick(settings.PAGE_SIZE)
+            return SavedMoment.objects.filter(screenshot_generated=True).filter(
+                owner=self.request.user
             )
 
         return SavedMoment.objects.all()
@@ -25,3 +25,19 @@ class SavedMomentView(
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=False)
+    def random(self, request):
+        moments = (
+            SavedMoment.random.screenshot_generated()
+            .owned_by(self.request.user)
+            .pick(settings.PAGE_SIZE)
+        )
+
+        page = self.paginate_queryset(moments)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(moments, many=True)
+        return Response(serializer.data)
