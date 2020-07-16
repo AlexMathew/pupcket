9abc6ce905eedfb5d804db77b1535f7f0c4d4725
Alex Mathew <alexmathew003@gmail.com>
2020-07-16 22:58:49 +0530

    Use options as a property in driver

--- api/utils/screenshot/driver.py
+++ api/utils/screenshot/driver.py
@@ -1,25 +1,13 @@
 from selenium import webdriver
 
 
-def get_options():
-    options = webdriver.ChromeOptions()
-    options.add_argument("disable-infobars")
-    options.add_argument("--headless")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-setuid-sandbox")
-    options.add_argument("--window-size=1920, 1200")
-
-    return options
-
-
 class Driver:
     CHROMEDRIVER = "/usr/local/bin/chromedriver"
-
-    def __init__(self):
-        self._options = get_options()
+    USER_AGENT = (
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 "
+        "Safari/537.36"
+    )
 
     def __enter__(self):
         self._driver = webdriver.Chrome(self.CHROMEDRIVER, options=self._options)
@@ -27,3 +15,18 @@ class Driver:
 
     def __exit__(self, exc_type, exc_value, traceback):
         self._driver.close()
+
+    @property
+    def _options(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("disable-infobars")
+        options.add_argument(self.USER_AGENT)
+        options.add_argument("--headless")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-setuid-sandbox")
+        options.add_argument("--window-size=1920, 1200")
+
+        return options
