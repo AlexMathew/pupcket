4bd3affd8c29d802aac6b54d8fbe5836629eef87
Alex Mathew <alexmathew003@gmail.com>
2020-12-19 01:16:58 +0530

    Set up embedded tweets viewer

--- embed_viewer/src/Twitter.js
+++ embed_viewer/src/Twitter.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export default function Twitter() {
-  return <div></div>;
+export default function Twitter(props) {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(props.location.search);
+    const twitterUrl = urlParams.get("url");
+    if (twitterUrl) {
+      document.getElementsByClassName("pawcket-tweet")[0].innerHTML = "";
+      const tweetId = twitterUrl.replace(/\/+$/, "").split("/").slice(-1)[0];
+      window.twttr.widgets.createTweet(
+        tweetId,
+        document.getElementsByClassName("pawcket-tweet")[0],
+        {
+          align: "center",
+        }
+      );
+    }
+  }, [props.location.search]);
+
+  return (
+    <div
+      className="pawcket-tweet"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    ></div>
+  );
 }
