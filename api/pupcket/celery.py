3024489494763b51b6edd7fb5fc35daa749cf7f2
Alex Mathew <alexmathew003@gmail.com>
2020-12-25 15:23:59 +0530

    Fix conf in celery to remove soon-to-be deprecated stuff

--- api/pupcket/celery.py
+++ api/pupcket/celery.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
-from pupcket import settings
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pupcket.settings")
 
@@ -9,11 +9,9 @@ REDIS_URL = f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/0"
 
 app = Celery("pupcket")
 
-app.conf.update(
-    BROKER_URL=REDIS_URL,
-    CELERY_TASK_RESULT_EXPIRES=3600,
-    CELERY_TIMEZONE=settings.TIME_ZONE,
-    CELERY_IMPORTS=(),
-)
+app.conf.broker_url = REDIS_URL
+app.conf.result_expires = 3600
+app.conf.timezone = settings.TIME_ZONE
+app.conf.imports = ()
 
 app.autodiscover_tasks()
