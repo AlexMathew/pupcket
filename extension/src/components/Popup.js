4d94c90866a27ae8592d2a4675389bcf22609741
Alex Mathew <alexmathew003@gmail.com>
2021-01-08 23:15:25 +0530

    Add success/failure icons in popup

--- extension/src/components/Popup.js
+++ extension/src/components/Popup.js
@@ -3,17 +3,29 @@ import { withStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Backdrop from "@material-ui/core/Backdrop";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import ErrorIcon from "@material-ui/icons/Error";
 import { saveMoment } from "../utils/pupcket";
 
 const styles = (theme) => ({
   root: {
-    display: "grid",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     width: theme.spacing(30),
     height: theme.spacing(10),
   },
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
     color: "white",
+  },
+  success: {
+    fill: "rgb(0, 255, 0)",
+    background: "rgb(255, 255, 255)",
+  },
+  failure: {
+    fill: "rgb(255, 0, 0)",
+    background: "rgb(255, 255, 255)",
   },
 });
 
@@ -44,7 +56,14 @@ class Popup extends React.Component {
     return (
       <div className={classes.root}>
         <CssBaseline />
-        {!this.state.loading && this.state.saved ? "Saved" : "Error"}
+        {!this.state.loading && this.state.saved ? (
+          <CheckCircleIcon
+            style={{ fontSize: 40 }}
+            className={classes.success}
+          />
+        ) : (
+          <ErrorIcon style={{ fontSize: 40 }} className={classes.failure} />
+        )}
         <Backdrop className={classes.backdrop} open={this.state.loading}>
           <CircularProgress color="inherit" />
         </Backdrop>
