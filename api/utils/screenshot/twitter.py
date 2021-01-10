10f05a03ade790f33481ae6de82e1127cc90d1c4
Alex Mathew <alexmathew003@gmail.com>
2021-01-10 23:04:50 +0530

    Handle cleaning twitter /photo/ URLs

--- api/utils/screenshot/twitter.py
+++ api/utils/screenshot/twitter.py
@@ -1,10 +1,22 @@
+import re
+
 from .base import Screenshot
 
 
 class Twitter(Screenshot):
     SECTION_SELECTOR = "//div[@class='twitter-tweet twitter-tweet-rendered']"
+    PHOTO_URL_RE = r"\d+\/photo\/"
+
+    @classmethod
+    def _clean_photo_url(cls, url):
+        if re.search(cls.PHOTO_URL_RE, url):
+            index = url.rfind("photo/")
+            return url[:index]
+
+        return url
 
     def generate_viewer_url(self, url):
+        url = self._clean_photo_url(url)
         return f"https://viewer.getpawcket.com/twitter?url={url}"
 
     def crop_area(self, section):
