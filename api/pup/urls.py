122cd5eb287c8ab75f08ae84b6547744d1294d5a
Alex Mathew <alexmathew003@gmail.com>
2021-01-23 00:49:25 +0530

    Expose djoser auth APIs only for debug

--- api/pup/urls.py
+++ api/pup/urls.py
@@ -1,14 +1,17 @@
+from django.conf import settings
 from django.urls import include, path
-from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from .auth_views import SocialAuthView
 from .views import SavedMomentView
 
-urlpatterns = [
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.authtoken")),
-]
+urlpatterns = []
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("auth/", include("djoser.urls")),
+        path("auth/", include("djoser.urls.authtoken")),
+    ]
 
 router = DefaultRouter()
 router.register(r"moment", SavedMomentView, basename="moment")
