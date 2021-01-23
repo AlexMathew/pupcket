4144d18155aa9274ca3b22bf0194b493b13e450e
Alex Mathew <alexmathew003@gmail.com>
2021-01-24 00:22:11 +0530

    Use social auth endpoint in sign in success callback + remove unused stuff from SignIn component

--- extension/src/components/options/FirebaseAuth.js
+++ extension/src/components/options/FirebaseAuth.js
@@ -2,6 +2,9 @@ import React from "react";
 import "firebase/auth";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase, { auth } from "./firebase";
+import pupcket from "../../api/pupcket";
+import { AUTH_TOKEN_FIELD } from "../../constants";
+import { fetchMoments } from "../../utils/pupcket";
 
 class FirebaseAuth extends React.Component {
   uiConfig = {
@@ -11,8 +14,18 @@ class FirebaseAuth extends React.Component {
       firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
-      signInSuccessWithAuthResult: (authResult) => {
-        console.log(authResult.user.toJSON());
+      signInSuccessWithAuthResult: async (authResult) => {
+        const user = authResult.user.toJSON();
+        const payload = {
+          ...user.providerData[0],
+          firebaseUid: user.uid,
+        };
+        const response = await pupcket.post("/social/auth/", payload);
+        chrome.storage.local.set({
+          [AUTH_TOKEN_FIELD]: { ...response.data },
+        });
+        this.props.history.push("/");
+        fetchMoments();
       },
     },
   };
--- extension/src/components/options/SignIn.js
+++ extension/src/components/options/SignIn.js
@@ -6,14 +6,9 @@ import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { red } from "@material-ui/core/colors";
 import Copyright from "./Copyright";
 import FirebaseAuth from "./FirebaseAuth";
-import pupcket from "../../api/pupcket";
 import { AUTH_TOKEN_FIELD } from "../../constants";
-import { fetchMoments } from "../../utils/pupcket";
-// import GoogleAuth from "./auth/GoogleAuth";
-// import TwitterAuth from "./auth/TwitterAuth";
 
 export const styles = (theme) => ({
   root: {
@@ -40,36 +35,9 @@ export const styles = (theme) => ({
     width: "100%",
     marginTop: theme.spacing(1),
   },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    display: "grid",
-    placeItems: "center",
-  },
-  error: {
-    fontSize: theme.spacing(2),
-    color: red[500],
-  },
-  submitSpinner: {
-    color: "white",
-    width: `${theme.spacing(3)}px !important`,
-    height: `${theme.spacing(3)}px !important`,
-  },
 });
 
 class SignIn extends React.Component {
-  state = {
-    modalOpen: false,
-    modalUsername: "",
-    username: "",
-    password: "",
-    error: {
-      general: "",
-      username: "",
-      password: "",
-    },
-    submitting: false,
-  };
-
   componentDidMount() {
     chrome.storage.local.get([AUTH_TOKEN_FIELD], (result) => {
       const token = result[[AUTH_TOKEN_FIELD]];
@@ -77,61 +45,7 @@ class SignIn extends React.Component {
         this.props.history.push("/");
       }
     });
-
-    const state = this.props.location.state;
-    if (state) {
-      if (state.success) {
-        this.setState({
-          modalOpen: true,
-          modalUsername: state.username,
-        });
-      }
-    }
   }
-
-  handleModalClose = () => {
-    this.setState({ modalOpen: false });
-  };
-
-  onSubmitSuccess = (input, response) => {
-    const { username } = input;
-    const { auth_token } = response.data;
-    chrome.storage.local.set({
-      [AUTH_TOKEN_FIELD]: { auth_token, username },
-    });
-    this.props.history.push("/");
-    fetchMoments();
-  };
-
-  submit = (event) => {
-    event.preventDefault();
-    this.setState({ submitting: true });
-    const { username, password } = this.state;
-    const fields = ["username", "password"];
-
-    pupcket
-      .post("/auth/token/login/", {
-        username,
-        password,
-      })
-      .then((response) => {
-        this.onSubmitSuccess({ username }, response);
-      })
-      .catch((err) => {
-        if (err.response) {
-          const error = {};
-          const data = err.response.data;
-          error.general = data.non_field_errors;
-          fields.forEach((field) => {
-            error[[field]] = (data[[field]] || []).join(" ");
-          });
-          this.setState({ error });
-        }
-      })
-      .finally(() => {
-        this.setState({ submitting: false });
-      });
-  };
 
   render() {
     const { classes } = this.props;
@@ -155,7 +69,7 @@ class SignIn extends React.Component {
               Sign in to Pawcket
             </Typography>
             <div className={classes.authBlock}>
-              <FirebaseAuth />
+              <FirebaseAuth history={this.props.history} />
             </div>
             <Box mt={5}>
               <Copyright />
